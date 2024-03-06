f70e80c2bcd152a831dab08457be0d96e6a53a07
nocilascg <nicolas99cg@gmail.com>
2024-03-05 22:23:23 -0500

    primera entrega 1

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['main.py'])
+setup(console=['main.py'],data_files=[('src/front/img', ['src/front/img/pantalla.png', 'src/front/img/impresora.png'])],)
